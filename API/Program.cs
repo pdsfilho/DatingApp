0637431da2ea1251f0ef19f694efe0d5e21aaa8b
Paulo <p.dantas@outlook.com>
2024-12-03 14:04:46 -0300

    Configuration: Migrations and DB

--- API/Program.cs
+++ API/Program.cs
@@ -6,9 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<DataContext>(opt =>
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DatingConnection"));
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();

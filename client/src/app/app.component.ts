c8eedb786a591997e29543c35182461044db9700
Paulo <p.dantas@outlook.com>
2024-12-04 16:12:21 -0300

    COnfiguration: Adding Angular

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  http = inject(HttpClient);
+  title = 'DatingApp';
+  users: any;
+  
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  } 
+}

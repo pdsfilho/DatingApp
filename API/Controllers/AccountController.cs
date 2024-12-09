9f4056d06da4cf24b253595f47ce0c02bc24a225
Paulo <p.dantas@outlook.com>
2024-12-09 13:23:50 -0300

    Feature: Adding DTOs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,29 +2,38 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(DataContext context) : BaseApiController
 {
     [HttpPost("register")] //account/register
-    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDto)
     {
+        if (await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
+
         //Using the Hash to generate a new password. 
         //The "using" statement already disposes of the instance after is used.
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = registerDto.UserName.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
         return user;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
     }
 }

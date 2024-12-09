aea4fbabf40a3b3ff9e0929fc554d0b94b4e317c
Paulo <p.dantas@outlook.com>
2024-12-09 17:55:46 -0300

    Feature: adding login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -32,6 +32,25 @@ public class AccountController(DataContext context) : BaseApiController
         return user;
     }
 
+    [HttpPost("login")] // account/login
+    public async Task<ActionResult<AppUser>> Login (LoginDTO loginDto)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x => 
+        x.UserName == loginDto.UserName.ToLower());
+
+        if (user == null) return Unauthorized("Invalid Username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        //Compare the Hash that is just being encoded with the Hash of the database
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+        }
+        return user;
+    }
     private async Task<bool> UserExists(string username)
     {
         return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());

bb562f684657ac85aa98e0f5969bda3ebad8030e
Paulo <p.dantas@outlook.com>
2025-01-16 20:10:27 -0300

    Feature: error handling

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,37 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController(DataContext context) :  BaseApiController
+{
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return "teste";
+    }
+    [HttpGet("not-found")]
+    public ActionResult<AppUser> GetNotFound()
+    {
+        var user = context.Users.Find(-1);
+        
+        if (user == null) return NotFound();
+        
+        return user;
+    }
+    [HttpGet("server-error")]
+    public ActionResult<AppUser> GetServerError()
+    {
+        var user = context.Users.Find(-1) ?? throw new Exception("A bad thing has happened");
+        return user;
+    }
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest()
+    {
+        return BadRequest("This was not a good request");
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,9 @@ builder.Services.AddApplicationServices(builder.Configuration);
 
 builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
+
+//Configure the HTTP request pipeline
+app.UseMiddleware<ExceptionMiddleWare>();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
 .WithOrigins("http://localhost:4200", "https://localhost:4200"));
